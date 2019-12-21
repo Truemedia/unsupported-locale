b83ada72f06bc86d027f604af5761da979bdf34e
Wade Penistone <wadespackages@yahoo.com>
2019-12-21 17:06:23 +0000

    PSR log level names trans and write to file functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const {AWSTranslateJSON} = require('aws-translate-json')
+const jsonfile = require('jsonfile')
+
 // Data
 const translatorLocales = require('./data/translator_locales')
 
@@ -13,9 +15,20 @@ const sourceLocale = 'en';
 const { translateJSON } = new AWSTranslateJSON(awsConfig, sourceLocale, translatorLocales);
 
 translateJSON({
-    key1: "my text here",
-    key2: "other text",
-    key3: {
-        key4: "nested text"
-    }
-}).then(console.log);
+  // PSR-3 logging levels (https://www.php-fig.org/psr/psr-3/)
+  psr3: {
+    emergency: 'emergency',
+    alert: 'alert',
+    critical: 'critical',
+    error: 'error',
+    warning: 'warning',
+    notice: 'notice',
+    info: 'info',
+    debug: 'debug'
+  }
+}).then( (trans) => {
+  console.log(trans)
+  return jsonfile.writeFile('./dist/trans.json', trans)
+}).then(res => {
+  console.log('Write complete')
+}).catch(error => console.error(error))
