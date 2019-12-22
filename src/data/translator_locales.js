aab141e17e307058c49d0b88dae2d61130e7bc6e
Wade Penistone <wadespackages@yahoo.com>
2019-12-22 16:26:10 +0000

    Proof of concept reviewable/api po to object file

--- src/data/translator_locales.js
+++ src/data/translator_locales.js
@@ -1,3 +1,3 @@
 module.exports = [
-  "pt", "it", "es"
+  "pt", "it", "es", "ja", "en"
 ]
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const {AWSTranslateJSON} = require('aws-translate-json')
 const jsonfile = require('jsonfile')
+const vuei18nPo = require('vuei18n-po');
 
-// Data
 const translatorLocales = require('./data/translator_locales')
 
 const awsConfig = {
@@ -10,25 +10,64 @@ const awsConfig = {
     secretAccessKey: process.env.AWS_TRANSLATE_SECRET,
     region: process.env.AWS_TRANSLATE_REGION,
 }
-const sourceLocale = 'en';
 
-const { translateJSON } = new AWSTranslateJSON(awsConfig, sourceLocale, translatorLocales);
+const sourceLocale = 'en_GB';
+// Msg ids to review
+let msgids = []
 
-translateJSON({
-  // PSR-3 logging levels (https://www.php-fig.org/psr/psr-3/)
-  psr3: {
-    emergency: 'emergency',
-    alert: 'alert',
-    critical: 'critical',
-    error: 'error',
-    warning: 'warning',
-    notice: 'notice',
-    info: 'info',
-    debug: 'debug'
+vuei18nPo({ // Convert PO files to translation object
+    po: [
+      'src/translations/**/*.po' // PSR-3 logging levels (https://www.php-fig.org/psr/psr-3/)
+    ],
+    pluralRules: 'spec/data/out/choices.js',
+    messagesFile: 'generated/allInOne.json',
+    messagesDir: 'generated'
+}).then( (trans) => { // Merge empty translations with
+  let sourceTrans = trans[sourceLocale]
+  let targetLocales = Object.keys(trans)
+  targetLocales.splice(targetLocales.indexOf(sourceLocale), 1)
+
+  Object.entries(trans).map( ([locale, data]) => {
+    if (locale != sourceLocale) {
+      let {messages} = data
+
+      Object.entries(messages).map( ([msgid, msgstring]) => {
+        if (msgstring.trim() == '') { // No translation availble
+          msgids.push(msgid)
+        }
+      })
+    }
+  })
+
+  return {msgids, targetLocales, sourceTrans}
+}).then( ({msgids, targetLocales, sourceTrans}) => { // Extract and translate reviewable subset of translations
+  if (msgids.length > 0) {
+    let {messages} = sourceTrans
+    let subset = msgids.reduce((a, e) => (a[e] = messages[e], a), {});
+
+    console.log('test', supportedLocaleAlias(sourceLocale), targetLocales.map(locale => supportedLocaleAlias(locale)))
+    let {translateJSON} = new AWSTranslateJSON(awsConfig, supportedLocaleAlias(sourceLocale), targetLocales.map(locale => supportedLocaleAlias(locale)));
+    return translateJSON({ messages: subset })
+  } else {
+    throw new Error('No translations to review (all translations provided fulfilled)')
   }
 }).then( (trans) => {
-  console.log(trans)
   return jsonfile.writeFile('./dist/trans.json', trans)
 }).then(res => {
   console.log('Write complete')
 }).catch(error => console.error(error))
+
+
+// Get equivalent supported locale alias for API
+function supportedLocaleAlias(locale) {
+  let lang = locale.split('_')[0]
+  let apiLocale = null
+
+  if (translatorLocales.includes(locale)) {
+    apiLocale = locale
+  } else if (translatorLocales.includes(lang)) {
+    apiLocale = lang
+  }
+
+  return apiLocale
+}
